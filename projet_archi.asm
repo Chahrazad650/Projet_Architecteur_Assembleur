757b934df729b92acf9b28a38d7e122d2b35755f
DAHMANI chahrazad <Dahmanichahrazad4@gmail.com>
2025-05-22 22:43:46 +0200

    Last_Update projet_archi.asm

--- projet_archi.asm
+++ projet_archi.asm
@@ -1,12 +1,12 @@
 # Programme RISC-V : Recherche du maximum entre deux entiers
 # Auteur : DAHMANI Chahrazad - DJELLAB Sarah - MGHEZZI Ines
 # Description : Ce programme compare deux entiers et affiche le plus grand.
-# Environnement utilisé : RARS
+# Environnement utilisÃ© : RARS
 
 #messages pour l'affichage
 .data
 message_entier1:   .string "Entrez le premier entier : " 
-message_entier2:   .string "Entrez le deuxième entier : "
+message_entier2:   .string "Entrez le deuxiÃ¨me entier : "
 message_max:       .string "Le maximum est : " 
 
 .text
@@ -14,21 +14,21 @@ message_max:       .string "Le maximum est : "
 
 main:
     # Afficher le message pour le premier entier printf("lire le premier entier ")
-    li a7, 4                # Syscall pour afficher une chaîne
+    li a7, 4                # Syscall pour afficher une chaÃ®ne
     la a0, message_entier1  # Charger l'adresse du premier message dans a0
-    ecall                   # afficher le message (Exécuter l'appel système)
+    ecall                   # afficher le message (ExÃ©cuter l'appel systÃ¨me)
 
     # Lire le premier entier
     li a7, 5                 
     ecall                   
     addi s0, a0, 0          
 
-    # Afficher le message pour le deuxième entier printf("lire le deuxieme entier ")
+    # Afficher le message pour le deuxiÃ¨me entier printf("lire le deuxieme entier ")
     li a7, 4                
     la a0, message_entier2  
     ecall                   
 
-    # Lire le deuxième entier
+    # Lire le deuxiÃ¨me entier
     li a7, 5                
     ecall                   
     addi s1, a0, 0          
@@ -36,11 +36,11 @@ main:
     # Comparaison des deux entiers
     # Si s0>=s1, alorss2 =s0
     # Sinon, s2=s1
-    bge s0, s1, entier1_est_plus_grand  # Si s0>=s1, saute à entier1_est_plus_grand
+    bge s0, s1, entier1_est_plus_grand  # Si s0>=s1, saute Ã  entier1_est_plus_grand
 
 entier2_est_plus_grand:
-    addi s2, s1, 0          # s2=s1 (copie de s1)
-    j afficher_max          # Saute à afficher_max
+    addi s2, s1, 0          # s2=s1
+    j afficher_max          # Saute Ã  afficher_max
 
 entier1_est_plus_grand:
     addi s2, s0, 0          # resultat est s0
@@ -59,3 +59,4 @@ afficher_max:
     # Fin du programme
     li a7, 10               
     ecall
+    # Merci pour votre attention
